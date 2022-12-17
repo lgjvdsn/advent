c82a290a1cbaa97d4dad9b8295558927a5c8ff75
lg <lg@lg-desktop.lg-home>
2022-12-17 12:10:09 -0500

    Fix get_day to exit gracefully when no args are passed

--- 22/py/get_day.py
+++ 22/py/get_day.py
@@ -8,6 +8,9 @@ parser.add_argument("-d", "--day", help="Please input the day as a number")
 parser.add_argument("-s", "--session", help="Please copy your session and pass it here")
 
 args = parser.parse_args()
+if not args.day or not args.session:
+    print("Please input -d for the day and -s for your session token")
+    exit()
 
 url = f'https://adventofcode.com/2022/day/{args.day}/input'
 headers = {'cookie': f'session={args.session}'}
